fe01d22fefbe8de29afda03d224167b817fbd95c
U17 <andrewmcbryde100@gmail.com>
2017-11-01 11:06:41 -0400

    fixed Fortran partition, issue dealt with fortran arrays starting at 1

--- Fortran/diff.f90
+++ Fortran/diff.f90
@@ -2,7 +2,7 @@ program diff
 
         implicit none
 
-        integer, parameter :: N = 30
+        integer, parameter :: N = 10
         
 
         real(kind=8) :: mTotal
@@ -20,6 +20,8 @@ program diff
 
         real :: coefficient
 
+        logical      :: partition
+
         integer :: i,j,k
         time = 0
 
@@ -32,25 +34,26 @@ program diff
         conMin = 0.0
         tStep  = hval/mSpeed
 
+        partition = .true.
 
         coefficient = (tStep*D)/(hval*hval)
 
         do i = 1, N
                 do j = 1, N
                         do k = 1, N
-                              room(i,j,k) = 0!((i-1)*N*N)+((j-1)*N)+k-1
+                                if (j==((N/2)+1) .and. i>=((N/2)+1) .and. partition) then
+                                        room(i,j,k) = -1.0
+                                else
+                                        room(i,j,k) = 0.0
+                                end if
+                                !print *, room(i,j,k)
                         end do
+                        !print *
                 end do
+                !print *
         end do
 
-        do i = 1, N
-                do j = 1, N
-                        do k = 1, N
-                                 !Print *, room(i,j,k), i, j, k
-                        end do
-                end do
-        end do
-
+        print *, (-1 == -1.0)
 
         do i = 1, 6
                 dCon(i) = 0
@@ -69,54 +72,57 @@ do while ((conMin/conMax) .lt. 0.99)
         do i = 1, N
                 do j = 1, N
                         do k = 1, N
-                                if (k==N) then
-                                        dCon(1) = 0
-                                else
-                                        dcon(1) = (room(i,j,k)-room(i,j,k+1)) * coefficient
-                                        room(i,j,k) = room(i,j,k) - dCon(1)
-                                        room(i,j,k+1) = room(i,j,k+1) + dCon(1)
+                                
+                                if (room(i,j,k) .ne. -1) then
+                                       
+                                        if (k==N .or. room(i,j,k+1) == -1) then
+                                                dCon(1) = 0
+                                        else
+                                                dcon(1) = (room(i,j,k)-room(i,j,k+1)) * coefficient
+                                                room(i,j,k) = room(i,j,k) - dCon(1)
+                                                room(i,j,k+1) = room(i,j,k+1) + dCon(1)
+                                        end if
+        
+                                        if (j==N .or. room(i,j+1,k) == -1) then
+                                                dCon(2) = 0
+                                        else
+                                                dcon(2) = (room(i,j,k)-room(i,j+1,k)) * coefficient
+                                                room(i,j,k) = room(i,j,k) - dCon(2)
+                                                room(i,j+1,k) = room(i,j+1,k) + dCon(2)
+                                        end if
+        
+                                        if (i==N .or. room(i+1,j,k) == -1) then
+                                                dCon(3) = 0 
+                                        else
+                                                dcon(3) = (room(i,j,k)-room(i+1,j,k)) * coefficient
+                                                room(i,j,k) = room(i,j,k) - dCon(3)
+                                                room(i+1,j,k) = room(i+1,j,k) + dCon(3)
+                                        end if
+        
+                                        if (k==1 .or. room(i,j,k-1) == -1) then
+                                                dCon(4) = 0
+                                        else
+                                                dcon(4) = (room(i,j,k)-room(i,j,k-1)) * coefficient
+                                                room(i,j,k) = room(i,j,k) - dCon(4)
+                                                room(i,j,k-1) = room(i,j,k-1) + dCon(4)
+                                        end if
+        
+                                        if (j==1 .or. room(i,j-1,k) == -1) then
+                                                dCon(5) = 0
+                                        else
+                                                dcon(5) = (room(i,j,k)-room(i,j-1,k)) * coefficient
+                                                room(i,j,k) = room(i,j,k) - dCon(5)
+                                                room(i,j-1,k) = room(i,j-1,k) + dCon(5)
+                                        end if
+        
+                                        if (i==1 .or. room(i-1,j,k) == -1) then
+                                                dCon(6) = 0
+                                        else
+                                                dcon(6) = (room(i,j,k)-room(i-1,j,k)) * coefficient
+                                                room(i,j,k) = room(i,j,k) - dCon(6)
+                                                room(i-1,j,k) = room(i-1,j,k) + dCon(6)
+                                        end if
                                 end if
-
-                                if (j==N) then
-                                        dCon(2) = 0
-                                else
-                                        dcon(2) = (room(i,j,k)-room(i,j+1,k)) * coefficient
-                                        room(i,j,k) = room(i,j,k) - dCon(2)
-                                        room(i,j+1,k) = room(i,j+1,k) + dCon(2)
-                                end if
-
-                                if (i==N) then
-                                        dCon(3) = 0 
-                                else
-                                        dcon(3) = (room(i,j,k)-room(i+1,j,k)) * coefficient
-                                        room(i,j,k) = room(i,j,k) - dCon(3)
-                                        room(i+1,j,k) = room(i+1,j,k) + dCon(3)
-                                end if
-
-                                if (k==1) then
-                                        dCon(4) = 0
-                                else
-                                        dcon(4) = (room(i,j,k)-room(i,j,k-1)) * coefficient
-                                        room(i,j,k) = room(i,j,k) - dCon(4)
-                                        room(i,j,k-1) = room(i,j,k-1) + dCon(4)
-                                end if
-
-                                if (j==1) then
-                                        dCon(5) = 0
-                                else
-                                        dcon(5) = (room(i,j,k)-room(i,j-1,k)) * coefficient
-                                        room(i,j,k) = room(i,j,k) - dCon(5)
-                                        room(i,j-1,k) = room(i,j-1,k) + dCon(5)
-                                end if
-
-                                if (i==1) then
-                                        dCon(6) = 0
-                                else
-                                        dcon(6) = (room(i,j,k)-room(i-1,j,k)) * coefficient
-                                        room(i,j,k) = room(i,j,k) - dCon(6)
-                                        room(i-1,j,k) = room(i-1,j,k) + dCon(6)
-                                end if
-
                         end do
                 end do
         end do
@@ -129,11 +135,11 @@ do while ((conMin/conMax) .lt. 0.99)
                 do j = 1, N
                         do k = 1, N
 
-                        if (room(i,j,k) < conMin) then
+                        if (room(i,j,k) < conMin .and. room(i,j,k) .ne. -1) then
                                 conMin = room(i,j,k)
                         end if
 
-                        if (room(i,j,k) > conMax) then
+                        if (room(i,j,k) > conMax .and. room(i,j,k) .ne. -1) then
                                 conMax = room(i,j,k)
                         end if
 
--- Python/diff.py
+++ Python/diff.py
@@ -12,7 +12,16 @@ conMin = 1.0
 tStep = hval/mSpeed
 time = 0.0
 
+partition = False
+
 room = [[[0.0 for k in range(N)]for j in range(N)]for i in range(N)]
+
+for i in range(N):
+    for j in range(N):
+        for k in range(N):
+            if (j==(N/2) and i>=(N/2) and partition):
+                room[i][j][k] = -1.0
+
 
 dCon = [0.0 for i in range(6)]
 
@@ -27,55 +36,55 @@ while (conMin/conMax < 0.99):
     for i in range(N):
         for j in range(N):
             for k in range(N):
-
-                if (k==N-1):
-                    dCon[0]
-                else:
-                    
-                    dCon[0] = (room[i][j][k] - room[i][j][k+1]) * coefficient
-                    room[i][j][k] = room[i][j][k] - dCon[0]
-                    room[i][j][k+1] = room[i][j][k+1] + dCon[0]
-                    
-                if (j==N-1):
-                    dCon[1]
-                else:
-
-                    dCon[1] = (room[i][j][k] - room[i][j+1][k]) * coefficient
-                    room[i][j][k] = room[i][j][k] - dCon[1]
-                    room[i][j+1][k] = room[i][j+1][k] + dCon[1]
-
-                if (i==N-1):
-                    dCon[2]
-                else:
- 
-                    dCon[2] = (room[i][j][k] - room[i+1][j][k]) * coefficient
-                    room[i][j][k] = room[i][j][k] - dCon[2]
-                    room[i+1][j][k] = room[i+1][j][k] + dCon[2]
-
-                if (k==0):
-                    dCon[3]
-                else:
-                    
-                    dCon[3] = (room[i][j][k] - room[i][j][k-1]) * coefficient
-                    room[i][j][k] = room[i][j][k] - dCon[3]
-                    room[i][j][k-1] = room[i][j][k-1] + dCon[3]
-
-                if (j==0):
-                    dCon[4]
-                else:
-                    
-                    dCon[4] = (room[i][j][k] - room[i][j-1][k]) * coefficient
-                    room[i][j][k] = room[i][j][k] - dCon[4]
-                    room[i][j-1][k] = room[i][j-1][k] + dCon[4]
-
-                if (i==0):
-                    dCon[5]
-                else:
-                   
-                    dCon[5] = (room[i][j][k] - room[i-1][j][k]) * coefficient
-                    room[i][j][k] = room[i][j][k] - dCon[5]
-                    room[i-1][j][k] = room[i-1][j][k] + dCon[5] 
-
+                if (room[i][j][k] != -1):
+                    if (k==N-1 or room[i][j][k+1] == -1.0):
+                        dCon[0]
+                    else:
+                        
+                        dCon[0] = (room[i][j][k] - room[i][j][k+1]) * coefficient
+                        room[i][j][k] = room[i][j][k] - dCon[0]
+                        room[i][j][k+1] = room[i][j][k+1] + dCon[0]
+                        
+                    if (j==N-1 or room[i][j+1][k] == -1):
+                        dCon[1]
+                    else:
+    
+                        dCon[1] = (room[i][j][k] - room[i][j+1][k]) * coefficient
+                        room[i][j][k] = room[i][j][k] - dCon[1]
+                        room[i][j+1][k] = room[i][j+1][k] + dCon[1]
+    
+                    if (i==N-1 or room[i+1][j][k] == -1):
+                        dCon[2]
+                    else:
+     
+                        dCon[2] = (room[i][j][k] - room[i+1][j][k]) * coefficient
+                        room[i][j][k] = room[i][j][k] - dCon[2]
+                        room[i+1][j][k] = room[i+1][j][k] + dCon[2]
+    
+                    if (k==0 or room[i][j][k-1] == -1):
+                        dCon[3]
+                    else:
+                        
+                        dCon[3] = (room[i][j][k] - room[i][j][k-1]) * coefficient
+                        room[i][j][k] = room[i][j][k] - dCon[3]
+                        room[i][j][k-1] = room[i][j][k-1] + dCon[3]
+    
+                    if (j==0 or room[i][j-1][k] == -1):
+                        dCon[4]
+                    else:
+                        
+                        dCon[4] = (room[i][j][k] - room[i][j-1][k]) * coefficient
+                        room[i][j][k] = room[i][j][k] - dCon[4]
+                        room[i][j-1][k] = room[i][j-1][k] + dCon[4]
+    
+                    if (i==0 or room[i-1][j][k] == -1):
+                        dCon[5]
+                    else:
+                       
+                        dCon[5] = (room[i][j][k] - room[i-1][j][k]) * coefficient
+                        room[i][j][k] = room[i][j][k] - dCon[5]
+                        room[i-1][j][k] = room[i-1][j][k] + dCon[5] 
+                
 
 
     conMax = room[0][0][0]
@@ -84,9 +93,9 @@ while (conMin/conMax < 0.99):
     for i in range(N):
         for j in range(N):
             for k in range(N):
-                if conMax < room[i][j][k]:
+                if conMax < room[i][j][k] and room[i][j][k] != -1:
                     conMax = room[i][j][k]
-                if conMin > room[i][j][k]:
+                if conMin > room[i][j][k] and room[i][j][k] != -1:
                     conMin = room[i][j][k]
 
 
